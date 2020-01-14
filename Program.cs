22fd69c9b845437aeecd7926d78c89578ad2afe7
unknown <frankruizreyes@hotmail.com>
2020-01-14 13:44:42 +0100

    hola

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Antonio
+{
+    class Program
+    {
+
+      
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hola soy una bella princesa en busca del amor");
+
+            Console.ReadLine();
+
+        }
+    }
+    
+}
